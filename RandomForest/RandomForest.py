fb9f665ec4d8bb13ac2469d60c4b98b7e15c6403
ikbal <73275697+ikbal-5@users.noreply.github.com>
2023-10-26 12:49:09 +0300

    RandomForest.py

--- RandomForest/RandomForest.py
+++ RandomForest/RandomForest.py
@@ -12,4 +12,16 @@ y = df.iloc[:,1].values.reshape(-1,1)
 from sklearn.ensemble import RandomForestRegressor
 rf = RandomForestRegressor(n_estimators=100,random_state=42)
 rf.fit(x,y)
-print("7.8 Sevşyesinde fiyatın ne kadar olduğu: ",rf.predict([[7.8]]))
+print("7.8 Seviyesinde fiyatın ne kadar olduğu: ",rf.predict([[7.8]]))
+
+
+#Modelin performans değerlendirmesi
+from sklearn.ensemble import RandomForestRegressor
+rf = RandomForestRegressor(n_estimators=100,random_state=42)
+rf.fit(x,y)
+y_head = rf.predict(x)
+
+
+from sklearn.metrics import r2_score
+
+print("r_score: ", r2_score(y,y_head))
