f201fec7c2d1d97947e3492588a97bffb5b29e5f
ikbal <73275697+ikbal-5@users.noreply.github.com>
2023-12-14 11:35:13 +0300

    Add files via upload

--- RandomForest/RandomForest.py
+++ RandomForest/RandomForest.py
@@ -1,27 +1,27 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import pandas as pd
+import numpy as np
+data =pd.read_csv("data.csv")
+data.drop(["id","Unnamed: 32"], axis=1,inplace=True)
+data.diagnosis = [ 1 if each == "M" else 0 for each in data.diagnosis]
+y = data.diagnosis.values
+x_data =data.drop(["diagnosis"],axis=1)
 
 
-df = pd.read_csv("DesicionTree.csv",sep=";",header = None)
-x = df.iloc[:,0].values.reshape(-1,1)
-y = df.iloc[:,1].values.reshape(-1,1)
+x = (x_data - np.min(x_data)) / (np.max(x_data)-np.min(x_data))
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.15,random_state=42)
+
+#Decision Tree
+from sklearn.tree import DecisionTreeClassifier
+dt = DecisionTreeClassifier()
+dt.fit(x_train,y_train)
+print("decision tree score: ",dt.score(x_test,y_test))
 
 
-#randomforest
-from sklearn.ensemble import RandomForestRegressor
-rf = RandomForestRegressor(n_estimators=100,random_state=42)
-rf.fit(x,y)
-print("7.8 Seviyesinde fiyatın ne kadar olduğu: ",rf.predict([[7.8]]))
+#Random Forest
+from sklearn.ensemble import RandomForestClassifier
 
+rf= RandomForestClassifier(n_estimators=100,random_state=1)
+rf.fit(x_train,y_train)
+print("Random Forest Algorithm Result:",rf.score(x_test,y_test))
 
-#Modelin performans değerlendirmesi
-from sklearn.ensemble import RandomForestRegressor
-rf = RandomForestRegressor(n_estimators=100,random_state=42)
-rf.fit(x,y)
-y_head = rf.predict(x)
-
-
-from sklearn.metrics import r2_score
-
-print("r_score: ", r2_score(y,y_head))
